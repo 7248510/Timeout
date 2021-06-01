b672255f4cda5882c36f490e1677a90ea0bf6d0a
Caleb K <56181475+7248510@users.noreply.github.com>
2021-05-31 21:16:31 -0400

    Bug fixes and Win32 API
    Timeout now works with Win32 API & I fixed some code bugs!

--- Timeout.c
+++ Timeout.c
@@ -7,24 +7,41 @@ void convertHours(int hour, int* returnCalulation) {
     return;
 }
 int winShutdown(int systemTime) {
-    printf("%d", systemTime);
-    //typedef char* machineName, *LPSTR;
+    HANDLE hToken; //handle the process tokens
+    TOKEN_PRIVILEGES tkp; //pointer to the token structure
+    //^= get the current process token handle to get the correct privleges to shut down the system
+    if (!OpenProcessToken(GetCurrentProcess(),TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)) {
+        return false;
+    }
+        
+    //Get the LUID? for the shutdown privilege.
+    LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid);
+    tkp.PrivilegeCount = 1; //One privilege to set
+    tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED; //Enable Privileges
+    //Get the shutdown privilege
+    AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, (PTOKEN_PRIVILEGES) NULL,0);
+    if (GetLastError() != ERROR_SUCCESS) {
+        return EXIT_FAILURE;
+    }
     char* machineName = NULL; //Current machine
-    //typedef char* machineMessage, *LPSTR;
-    char* machineMessage = NULL;
-    //no message is required/to be displayed to the user since they know the PC's shutting down
-    DWORD shutdownTime = systemTime;
+    char* machineMessage = "TimeOut is shutting down your computer";
+    printf("\nSHUTDOWN TIME: %d", systemTime);
+    //Message will be displayed
     bool forceAppsClosed = false; //Displays box to close applications preventing dataloss
     bool restartImmediately = false; //Safely powers down the system & flushes the caches
-    bool systemShutdown = InitiateSystemShutdownA(machineName, machineMessage, shutdownTime, forceAppsClosed, restartImmediately);
-    //InitiateSystemShutdownA = bool value
-    if (systemShutdown == 0) {
-        if (printf("Shutting down Windows in %d", systemTime) < 0) {
+    bool systemShutdown = InitiateSystemShutdown(machineName, machineMessage, systemTime, forceAppsClosed, restartImmediately);
+    if (!systemShutdown) {
+        if (printf("\nFunction succesfully executes. Shutting down in: %d", systemTime) < 0) {
             return EXIT_FAILURE;
         }
         return EXIT_SUCCESS;
     }
-    return 0;
+    if (systemShutdown > 0 && systemShutdown < 0) {
+        return EXIT_FAILURE;
+    }
+    tkp.Privileges[0].Attributes =0;
+    AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, (PTOKEN_PRIVILEGES) NULL, 0);
+    return EXIT_SUCCESS;
 }
 int main(void) {
     if (puts("Starting shutdown.") == EOF) {
@@ -33,17 +50,17 @@ int main(void) {
     }
     //Operations begin
     int returnCalulation = 0;
+    int twoHours = 2;
     //const hourConvert = 2; //Value does not change. It eventually will;
-    const int instantShutdown = 0; //Testing the program in a W10 Virtual Machine
-    convertHours(instantShutdown, &returnCalulation);
+    //const int instantShutdown = 0; //Testing the program in a W10 Virtual Machine
+    int testSeconds = 1; //Testing the program in a W10 Virtual Machine
+    convertHours(twoHours, &returnCalulation);
     if (printf("2 hours in seconds is %d", returnCalulation) < 0) {
         return EXIT_FAILURE;
     }
-    if (printf("\nSystem is shutting down in: %d", returnCalulation) < 0) {
-        return 1;
-    }
-    if (winShutdown(returnCalulation) > 0) {
+    if (winShutdown(testSeconds) > 0) {
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
 }
+
