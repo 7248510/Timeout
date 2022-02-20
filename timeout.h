99dc61855fb4e5eca73902d2b4786b60610e9d55
Caleb <56181475+7248510@users.noreply.github.com>
2022-02-19 22:44:18 -0500

    Update timeout.h

--- timeout.h
+++ timeout.h
@@ -8,55 +8,22 @@
 #include <fmt/core.h>
 #include <compare>
 #include <iostream>
+
 class timeout {
 public:
     static void printWelcome();
 };
 
-void timeout::printWelcome() {
-    std::string welcome{"Hello you started TimeOut"};
-    std::cout << fmt::format("{}",welcome) << std::endl;
-}
-
-
-
-
-
 class timeConversion {
 public:
     int errorMessage {0};
-
-    timeConversion() {
-        errorMessage = -1;
-        std::cout << fmt::format("Number not declared exiting with error code {}", errorMessage);
-        exit(errorMessage);
-    }
-
-    timeConversion(int tmp) {
-        timeInput = tmp;
-    }
-
-
-    void message() {
-        std::cout << fmt::format("Please enter an hour. You would like converted to seconds.") << std::endl;
-    }
-    void convert() {
-        std::strong_ordering result{ timeInput <=> 0 };
-        if (result == std::strong_ordering::equal) {
-            std::cout << "You entered zero. This is an invalid value.";
-        }
-
-        const unsigned int multiplyVal = 3600;
-        unsigned int complete = timeInput * multiplyVal;
-
-        std::cout << complete;
-    }
-
+    timeConversion();
+    void setValue(int);
+    void message();
+    void convert();
+    ~timeConversion();
 private:
-    int timeInput;
+    int timeInput{};
 };
-
-
-
 
 #endif //TEST_TIMEOUT_H
