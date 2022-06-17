47021fc915c66c66b98c489dc1bbb9e7cc20e3cc
Caleb <56181475+7248510@users.noreply.github.com>
2022-06-16 22:19:28 -0400

    Delete Timeout.h

--- Timeout.h
+++
@@ -1,7 +0,0 @@
-#pragma comment(lib, "ADVAPI32")
-#pragma comment(lib, "USER32")
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h> 
-#define WIN32_LEAN_AND_MEAN //Define WIN32_LEAN_AND_MEAN to exclude APIs such as Cryptography, DDE, RPC, Shell, and Windows Sockets.
-#include <stdbool.h>
